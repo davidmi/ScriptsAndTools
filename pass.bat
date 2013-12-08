3a2b286394511147b2b76f98095257210f64c711
David M Iserovich <david.iserovich@gmail.com>
2013-12-08 15:03:14 -0500

    Create pass.bat

---
+++ pass.bat
@@ -0,0 +1,17 @@
+@echo off
+set PASSWORD_STORE=C:\Users\David\.password-store
+if not "%1" == "" (
+	REM ## COPY THE KEY TO THE CLIPBOARD ##
+	echo gpg -d %PASSWORD_STORE%\%1
+	gpg -d %PASSWORD_STORE%\%1 | clip
+
+	REM ## WAIT 30 SECONDS ## 
+	PING 1.1.1.1 -n 1 -w 30000 >NUL
+	
+	REM ## CLEAR THE CLIPBOARD ##
+	echo "" | clip
+)
+if not defined 1 (
+	REM ## SHOW THE PASSWORD STORE DIRECTORY LISTING ##
+	tree /f %PASSWORD_STORE%
+) 
