4b2f56fc660aa776155438c98859e814c013d197
David Iserovich <david.iserovich@gmail.com>
2014-08-19 14:13:26 -0400

    Added implicit .gpg

--- pass.bat
+++ pass.bat
@@ -3,7 +3,7 @@ set PASSWORD_STORE=C:\Users\%USERNAME%\.password-store
 if not "%1" == "" (
 	REM ## COPY THE KEY TO THE CLIPBOARD ##
 	echo gpg -d %PASSWORD_STORE%\%1
-	gpg -d %PASSWORD_STORE%\%1 | clip
+	gpg -d %PASSWORD_STORE%\%1.gpg | clip
 
 	REM ## WAIT 30 SECONDS ## 
 	PING 1.1.1.1 -n 1 -w 30000 >NUL
