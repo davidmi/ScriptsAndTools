778a50ec1b08e99d3a5bc99de84667bf98665c7d
David Iserovich <david.iserovich@gmail.com>
2014-09-07 23:36:54 -0400

    Made syntax follow unix version more closely

--- pass.bat
+++ pass.bat
@@ -1,16 +1,25 @@
 @echo off
 set PASSWORD_STORE=C:\Users\%USERNAME%\.password-store
-if not "%1" == "" (
-	REM ## COPY THE KEY TO THE CLIPBOARD ##
-	echo gpg -d %PASSWORD_STORE%\%1
-	gpg -d %PASSWORD_STORE%\%1.gpg | clip
+if "%1" == "show" (
+	if "%2" == "" (
+		echo Please enter a password file name: pass show [filename]
+	)
+	if not "%2" == "" (
+		REM ## COPY THE KEY TO THE CLIPBOARD ##
+		echo gpg -d %PASSWORD_STORE%\%2
+		gpg -d %PASSWORD_STORE%\%2.gpg | clip
 
-	REM ## WAIT 10 SECONDS ## 
-	PING 1.1.1.1 -n 1 -w 10000 >NUL
-	
-	REM ## CLEAR THE CLIPBOARD ##
-	echo "" | clip
+		REM ## WAIT 10 SECONDS ## 
+		PING 1.1.1.1 -n 1 -w 10000 >NUL
+		
+		REM ## CLEAR THE CLIPBOARD ##
+		echo "" | clip
+	)
 )
+if "%1" == "generate" (
+	echo Sorry, password generation is not yet supported
+)
+
 if "%1" == "" (
 	REM ## SHOW THE PASSWORD STORE DIRECTORY LISTING ##
 	tree /f %PASSWORD_STORE%
